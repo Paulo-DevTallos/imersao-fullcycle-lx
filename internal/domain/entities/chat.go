81c17194d41e5309a68bf806e568d59a84937ce5
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-17 21:37:41 -0300

    feat: create business rules

--- internal/domain/entities/chat.go
+++ internal/domain/entities/chat.go
@@ -1,7 +1,20 @@
 package entities
 
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
 type ChatConfig struct {
-	Model *Model
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
 }
 
 type Chat struct {
@@ -13,4 +26,76 @@ type Chat struct {
 	Status               string
 	TokenUsage           int
 	Config               *ChatConfig
+}
+
+func NewChat(userID string, initialSystemMessage *Message, chatConfig *ChatConfig) (*Chat, error) {
+	chat := &Chat{
+		ID:                   uuid.New().String(),
+		UserID:               userID,
+		InitialSystemMessage: initialSystemMessage,
+		Status:               "active",
+		Config:               chatConfig,
+		TokenUsage:           0,
+	}
+	chat.AddMessage(initialSystemMessage)
+
+	if err := chat.Validade(); err != nil {
+		return nil, err
+	}
+	return chat, nil
+}
+
+func (c *Chat) Validade() error {
+	if c.UserID == "" {
+		return errors.New("User id is empty")
+	}
+
+	if c.Status != "active" && c.Status != "ended" {
+		return errors.New("Invalid status")
+	}
+
+	if c.Config.Temperature < 0 || c.Config.Temperature < 2 {
+		return errors.New("Invalid temperature")
+	}
+
+	//... more validations for config
+	return nil
+}
+
+// criando metodo que que cria um novo chat
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("Chat is ended. No more messages allowed")
+	}
+	for {
+		if c.Config.Model.GetMaxTokens() >= m.GetQuantityTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+			c.RefreshTokenUsage()
+			break
+		}
+		c.ErasedMessages = append(c.ErasedMessages, c.Messages[0])
+		c.Messages = c.Messages[1:]
+		c.RefreshTokenUsage()
+	}
+	return nil
+}
+
+func (c *Chat) GetMessages() []*Message {
+	return c.Messages
+}
+
+func (c *Chat) CountMessages() int {
+	return len(c.Messages)
+}
+
+func (c *Chat) End() {
+	c.Status = "ended"
+}
+
+// lançando refresh tokenUsage para ver quantos foram utilizados no chat
+func (c *Chat) RefreshTokenUsage() {
+	c.TokenUsage = 0
+	for m := range c.Messages {
+		c.TokenUsage += c.Messages[m].GetQuantityTokens()
+	}
 }
